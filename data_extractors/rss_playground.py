1692efb59c507eb459db4cdee9ac2c615cb9a65d
DiscoDoggy <akaattarilim@gmail.com>
2024-12-28 11:53:22 -0800

    Stashing temporarily

--- data_extractors/rss_playground.py
+++ data_extractors/rss_playground.py
@@ -1,6 +1,7 @@
 import feedparser
 import requests
 from all_rss_links_complete import all_rss_links_complete as RSS_FEED_LINKS
+from readability import Document
 
 # link = "https://github.blog/engineering.atom"
 # link = "http://tech.finn.no/atom.xml"
@@ -22,35 +23,37 @@ from all_rss_links_complete import all_rss_links_complete as RSS_FEED_LINKS
 #         print(tag["term"])
 #     print("\n")
 
-def test_for_broken_resources():
-    with open("non_working_feeds.txt", "w") as file:
-        for rss_feed_name in RSS_FEED_LINKS:
-            link = RSS_FEED_LINKS[rss_feed_name]
+# def test_for_broken_resources():
+#     with open("non_working_feeds.txt", "w") as file:
+#         for rss_feed_name in RSS_FEED_LINKS:
+#             link = RSS_FEED_LINKS[rss_feed_name]
             
-            try:
-                parsed_response = feedparser.parse(link)
+#             try:
+#                 parsed_response = feedparser.parse(link)
 
-                if len(parsed_response["channel"]) == 0:
-                    raise ValueError("Link did not work")
-            except:
-                print(f"{link} | Reason: Link did not work/site was down")
-                file.write(f"{link} | Reason: Link did not work/site was down\n")
-                continue
+#                 if len(parsed_response["channel"]) == 0:
+#                     raise ValueError("Link did not work")
+#             except:
+#                 print(f"{link} | Reason: Link did not work/site was down")
+#                 file.write(f"{link} | Reason: Link did not work/site was down\n")
+#                 continue
 
-            if "channel" not in parsed_response:
-                print(f"{link} | Reason: Channel tag or atom equivalent was not present")
-                file.write(f"{link} | Reason: Channel tag or atom equivalent was not present\n")
-            elif "items" not in parsed_response or len(parsed_response.entries) == 0:
-                print(f"{link} | Reason: Entries or items not present")
-                file.write(f"{link} | Reason: entries or items not present\n")
+#             if "channel" not in parsed_response:
+#                 print(f"{link} | Reason: Channel tag or atom equivalent was not present")
+#                 file.write(f"{link} | Reason: Channel tag or atom equivalent was not present\n")
+#             elif "items" not in parsed_response or len(parsed_response.entries) == 0:
+#                 print(f"{link} | Reason: Entries or items not present")
+#                 file.write(f"{link} | Reason: entries or items not present\n")
             
-            channel = parsed_response["channel"]
-            if "link" not in channel:
-                print(f"{link} | no link")
-def test_working_links():
-    #things i need to  check existence of 
-    # Description/subtitle
-    # categories
-    pass
+#             channel = parsed_response["channel"]
+#             if "link" not in channel:
+#                 print(f"{link} | no link")
+# def test_working_links():
+#     #things i need to  check existence of 
+#     # Description/subtitle
+#     # categories
+#     pass
 
-test_for_broken_resources()
+# test_for_broken_resources()
+
+
