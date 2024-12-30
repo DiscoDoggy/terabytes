c37c017ec9faf59a30a2920dcf00a5142e69998c
DiscoDoggy <akaattarilim@gmail.com>
2024-12-29 19:56:06 -0800

    Added logic to insert extracted blog tags to DB * Check if tag exists in the DB already     * If not insert tag to tags table * Insert blog id and tag id to blog_tags table

--- data_extractors/data_exporter.py
+++ data_extractors/data_exporter.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 from sqlalchemy import select
-from database_model import company_blog_site, company_blog_posts
+from database_model import company_blog_site, company_blog_posts, all_db_tags, blog_tags
 from database_connection import DatabaseConnection
 from blog_snippet import RSSBlogSnippet
 from rss_info import RSSInfo
@@ -62,9 +62,47 @@ class RSSBlogExporter(DataExporter):
                         title=blog.title,
                         description=blog.description,
                         publication_date=blog.publication_date,
-                        link=blog.link
+                        link=blog.link,
+                        content=blog.content
                     )
+                    .returning(index_elements=["id"])
                 )
 
-                conn.execute(insert_query)
+                blog_id = conn.execute(insert_query).first().id
                 conn.commit()
+
+                for tag in blog.tags:
+                    self.insert_tag_db(blog_id, tag)
+
+
+    def insert_tag_db(self, post_id, tag : str, db_conn):
+        cleaned_tag = tag.strip().capitalize()
+
+        query = (
+            select(all_db_tags.c.id)
+            .where(cleaned_tag == all_db_tags.c.name)
+        )
+
+        result = db_conn.execute(query).first()
+
+        tag_id = -1
+        if result is None:
+            query = (
+                insert(all_db_tags)
+                .values(name=tag)
+                .returning(index_elements=["id"])
+            )
+
+            tag_result = db_conn.execute(query).first()
+            tag_id = tag_result.id
+            db_conn.commit()
+        else:
+            tag_id = result.id
+        
+        insert_query = (
+            insert(blog_tags)
+            .values(blog_post_id=post_id, tag_id=tag_id)
+        )
+
+        db_conn.execute(insert_query)
+        db_conn.commit()
